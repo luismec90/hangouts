6b1c018ee090971c665b9aa6221a3df6063d04eb
Luis Montoya <luismec90@gmail.com>
2015-07-28 18:31:46 -0500

    Mejoras

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -1,4 +1,4 @@
-<h1>te han contactado de public-hangouts.com</h1>
+<h1>Te han contactado de public-hangouts.com</h1>
 <h4>Name: {{ $name }}</h4>
 <h4>Email: {{ $email }} </h4>
 <h4>Mensaje: {{ $userMessage }}</h4>
