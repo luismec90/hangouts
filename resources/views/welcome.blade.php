2a77bcbb0a52973a4f16e652b764581d2b76e190
Luis Montoya <luismec90@gmail.com>
2015-07-27 19:28:34 -0500

    Mejoras

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,150 @@
-<!DOCTYPE html>
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
 <html>
-    <head>
-        <title>Laravel</title>
+<head>
+    <title>Flat-Mate Website Template | Home :: w3layouts</title>
+    <link href="css/bootstrap.css" rel='stylesheet' type='text/css'/>
+    <link href="css/bootstrap.min.css" rel='stylesheet' type='text/css'/>
+    <link href="css/theme-style.css" rel='stylesheet' type='text/css'/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/x-icon" href="images/fav-icon.png"/>
 
-        <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    <!----webfonts---->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Fauna+One' rel='stylesheet' type='text/css'>
+    <!----//webfonts---->
 
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
+</head>
+<body>
+<!----start-bg---->
+<div class="bg">
+    <!----start-container---->
+    <div class="container">
+        <!---- start-header---->
+        <div class="row header">
+            <div class="row">
+                <div class="col-md-6">
+                    <!----start-logo---->
+                    <div class="well logo">
+                        <a href="#"><img src="/images/logo.png" title="Flatmate"/></a>
+                    </div>
+                    <!----//End-logo---->
+                </div>
+                <div class="well col-md-6">
+                    <!---start-header-right--->
+                    <div class="login-info well header-right">
+                        <!--<p><i><a href="#">Have an Account ?</a></i><a class="login btn-primary" href="#">Login</a></p> -->
+                    </div>
+                    <!---//End-header-right--->
+                </div>
             </div>
         </div>
-    </body>
+        <!---- //End-header---->
+        <!-----slide-banner---->
+        <div class="well slide-banner row">
+            <div class="slide-banner-left col-md-8">
+                <span> </span>
+
+                <h1>Lorem ipsum dolor sit amet</h1>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. </p>
+
+                <p><a class="btn btn-primary btn-lg b-btn" href="#"> Read More</a></p>
+            </div>
+            <div class="well slide-banner-right col-md-4">
+                <img src="images/slider-img.png" class="img-responsive" alt=""/>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+        <!-----//slide-banner---->
+    </div>
+</div>
+<!----start-top-grids---->
+<div class="top-grids">
+    <div class="container">
+        <div class="top-grid-left col-md-3">
+            <a href="#"><img src="images/icon2.png" class="img-responsive" title="doc"/></a>
+        </div>
+        <div class="top-grid-center col-md-7">
+            <h2>Lorem ipsum dolor sit amet</h2>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat.</p>
+        </div>
+        <div class="top-grid-right col-md-2">
+            <ul>
+                <li><a href="#"><span class="icon1"> </span></a></li>
+                <li><a href="#"><span class="icon2"> </span></a></li>
+                <div class="clearfix"></div>
+            </ul>
+        </div>
+    </div>
+</div>
+<!----//End-top-grids---->
+
+<!----start-contact---->
+<div class="contact">
+    <div class="container">
+        <p><i> </i></p>
+
+        <div class="row">
+            <form>
+                <div class="col-md-6 contact-text-box">
+                    <div>
+                        <span>Name<label>*</label></span>
+                        <input type="text" required/>
+                    </div>
+                    <div>
+                        <span>Email<label>*</label></span>
+                        <input type="text" required/>
+                    </div>
+                </div>
+                <div class="col-md-6 contact-text-textarea">
+                    <div>
+                        <span>Message<label>*</label></span>
+                        <textarea> </textarea>
+                    </div>
+                    <input class="btn btn-danger btn-lg" type="submit" Value="send"/>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<!----//End-contact---->
+
+<!----start-footer--->
+<div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 footer-left">
+                <ul>
+                    <li><a href="#"><img src="images/footer-logo.png" title="logo"/></a></li>
+                    <li><p>Template by <a href="http://w3layouts.com/">W3layouts</a></p></li>
+                </ul>
+            </div>
+            <div class="col-md-6 footer-right">
+                <ul>
+                    <li><a class="twitter" href="#"><span> </span></a></li>
+                    <li><a class="facebook" href="#"><span> </span></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<!----//End-footer--->
+<!----//End-container---->
+
+<!----requred-js-files---->
+<script src="/js/jquery.min.js"></script>
+<script src="/js/bootstrap.min.js"></script>
+<!----//requred-js-files---->
+</body>
 </html>
+
